8818a9a96c8ead03e91a357296e27908cc084cc8
francois <francoisrvespa@gmail.com>
2016-03-15 08:12:59 -0600

    modal

--- app/components/modal.jsx
+++ app/components/modal.jsx
@@ -1,11 +1,85 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Stylesheet, MediaQuery } from 'cassis';
 
 class Modal extends React.Component {
+
+  view;
+
+  css = {
+    'modal' : new Stylesheet(`
+      .reacted-modal {
+        position : absolute;
+        top : 0;
+        height : 100vh;
+        left : 0;
+        right : 0;
+        background : rgba(0, 0, 0, .6);
+      }
+
+      .reacted-modal--hidden {
+        top : -99999px
+      }
+    `),
+
+    box : new Stylesheet(`.reacted-modal-box {
+      position : absolute;
+      border : 5px solid black;
+      background : #fff;
+      top : 25%;
+      left : 25%;
+      right : 25%;
+      bottom : 25%;
+      z-index : 999;
+    }`)
+  };
+
+  style = document.querySelector('#cassis-style');
+
+  componentDidMount() {
+    this.view = ReactDOM.findDOMNode(this.refs.view);
+
+    if ( ! this.style ) {
+      this.style = document.createElement('style');
+      this.style.setAttribute('id', 'cassis-style');
+      this.style.appendChild(document.createTextNode([
+        this.css.modal.toString(),
+        this.css.box.toString()
+      ].join('\n')));
+
+      document.body.appendChild(this.style);
+    }
+  }
+
+  toggle (e) {
+    this.props.toggle(e);
+  }
+
+  blockToggle (e) {
+    e.stopPropagation();
+  }
+
   render () {
+    let show = false;
+
+    if ( ( 'show' in this.props ) ) {
+      show = this.props.show;
+    }
+
+    const classes = ['reacted-modal'];
+
+    if ( ! show ) {
+      classes.push('reacted-modal--hidden');
+    }
+
     return (
-      <div>Modal</div>
+      <div ref="view" className={ classes.join(' ') } onClick={::this.toggle}>
+        <div className="reacted-modal-box" onClick={::this.blockToggle}>
+          <h1>Hey!</h1>
+        </div>
+      </div>
     );
   }
 }
--- dist/components/modal.js
+++ dist/components/modal.js
@@ -10,6 +10,12 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _cassis = require('cassis');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -22,18 +28,73 @@ var Modal = function (_React$Component) {
   _inherits(Modal, _React$Component);
 
   function Modal() {
+    var _Object$getPrototypeO;
+
+    var _temp, _this, _ret;
+
     _classCallCheck(this, Modal);
 
-    return _possibleConstructorReturn(this, Object.getPrototypeOf(Modal).apply(this, arguments));
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Modal)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.css = {
+      'modal': new _cassis.Stylesheet('\n      .reacted-modal {\n        position : absolute;\n        top : 0;\n        height : 100vh;\n        left : 0;\n        right : 0;\n        background : rgba(0, 0, 0, .6);\n      }\n\n      .reacted-modal--hidden {\n        top : -99999px\n      }\n    '),
+
+      box: new _cassis.Stylesheet('.reacted-modal-box {\n      position : absolute;\n      border : 5px solid black;\n      background : #fff;\n      top : 25%;\n      left : 25%;\n      right : 25%;\n      bottom : 25%;\n      z-index : 999;\n    }')
+    }, _this.style = document.querySelector('#cassis-style'), _temp), _possibleConstructorReturn(_this, _ret);
   }
 
   _createClass(Modal, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      this.view = _reactDom2.default.findDOMNode(this.refs.view);
+
+      if (!this.style) {
+        this.style = document.createElement('style');
+        this.style.setAttribute('id', 'cassis-style');
+        this.style.appendChild(document.createTextNode([this.css.modal.toString(), this.css.box.toString()].join('\n')));
+
+        document.body.appendChild(this.style);
+      }
+    }
+  }, {
+    key: 'toggle',
+    value: function toggle(e) {
+      this.props.toggle(e);
+    }
+  }, {
+    key: 'blockToggle',
+    value: function blockToggle(e) {
+      e.stopPropagation();
+    }
+  }, {
     key: 'render',
     value: function render() {
+      var show = false;
+
+      if ('show' in this.props) {
+        show = this.props.show;
+      }
+
+      var classes = ['reacted-modal'];
+
+      if (!show) {
+        classes.push('reacted-modal--hidden');
+      }
+
       return _react2.default.createElement(
         'div',
-        null,
-        'Modal'
+        { ref: 'view', className: classes.join(' '), onClick: this.toggle.bind(this) },
+        _react2.default.createElement(
+          'div',
+          { className: 'reacted-modal-box', onClick: this.blockToggle.bind(this) },
+          _react2.default.createElement(
+            'h1',
+            null,
+            'Hey!'
+          )
+        )
       );
     }
   }]);
